8676e8f3a5f39d8e6498f9603734ba44d16fa529
aksatyam <ashis@cybertrontechnologies.com>
2018-12-16 21:22:49 +0530

    postlogin method added

--- src/providers/webservices/webservices.ts
+++ src/providers/webservices/webservices.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
+import "rxjs/add/operator/map";
 
 /*
   Generated class for the WebservicesProvider provider.
@@ -9,9 +11,58 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class WebservicesProvider {
+  public loading: any;
+  public BASE_URL: string = ' http://staging.c2sbook.org/c2s/';
+  public data: any;
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, public loadingCtrl: LoadingController, public headers: HttpHeaders) {
     console.log('Hello WebservicesProvider Provider');
+  }
+
+  presentLoading() {
+   this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+
+  }
+
+  dismissLoading() {
+    this.loading.dismiss();
+ 
+  }
+
+  postLogin(data) {
+    if (data == null) {
+      data = {
+        "byteImage":null,
+        "categoryId":null,
+        "dealId":null,
+        "deviceId":"5c0a774fe4b0c52289470a26",
+        "favouriteDealId":null,
+        "favouriteStoreId":null,
+        "language":"en",
+        "lat":"28.6295509",
+        "lon":"77.3769583",
+        "mobile":"8285604315",
+        "password":null,
+        "pwd":"puarsVPRq/Ps83aRv4m3/Q==",
+        "requestType":0,
+        "role":null,
+        "userCategory":null,
+        "userDistance":0.0,
+        "userId":null,
+        "userNotification":false,
+        "venderId":null
+      }
+    }
+    let url = `${this.BASE_URL}user/rest/login`;
+    this.http.post(url, data).subscribe(res => {
+      return res;
+    }, err => {
+      return err;
+    })
+
   }
 
 }
